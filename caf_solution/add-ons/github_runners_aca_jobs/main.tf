33aa0c5b9b5651b77da0cc87e86f4aa02033649b
jvdhaterdwestland <134271508+jvdhaterdwestland@users.noreply.github.com>
2023-08-16 08:09:01 +0000

    aca jobs

--- caf_solution/add-ons/github_runners_aca_jobs/main.tf
+++ caf_solution/add-ons/github_runners_aca_jobs/main.tf
@@ -2,21 +2,17 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.40.0"
     }
 
     azapi = {
       source  = "azure/azapi"
-      version = ">= 1.5.0"
     }
 
     azuread = {
       source  = "hashicorp/azuread"
-      version = ">= 2.7.0"
     }
     azurecaf = {
       source  = "aztfmod/azurecaf"
-      version = "~> 1.2.0"
     }
   }
   required_version = ">= 1.3.7"
