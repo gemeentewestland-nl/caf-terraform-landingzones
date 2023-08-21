2337c718a42479b250b5a052cf81d3eac1c5ecec
jvdhaterdwestland <134271508+jvdhaterdwestland@users.noreply.github.com>
2023-08-21 11:13:44 +0000

    aca jobs

--- caf_solution/add-ons/github_runners_aca_jobs/container_app_jobs.tf
+++ caf_solution/add-ons/github_runners_aca_jobs/container_app_jobs.tf
@@ -3,7 +3,7 @@ module "container_app_jobs" {
   for_each   = var.container_app_jobs
 
   name                                = each.value.name
-  container_app_environment_id        = can(each.value.container_app_environment_id) ? each.value.container_app_environment_id : module.container_app_environments[try(each.value.lz_key, var.landingzone_key)][each.value.container_app_environment_key].id
+  container_app_environment_id        = can(each.value.container_app_environment_id) ? each.value.container_app_environment_id : module.container_app_environments[try(each.value.lz_key, var.landingzone.key)][each.value.container_app_environment_key].id
   location                            = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined.resource_groups[try(each.value.resource_group.lz_key, var.landingzone.key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
   global_settings                     = local.global_settings
   settings                            = each.value
