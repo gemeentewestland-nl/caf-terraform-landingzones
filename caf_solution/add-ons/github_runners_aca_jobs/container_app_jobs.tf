d4c45e156208ae02fe7f4188b613958023dcac6e
jvdhaterdwestland <134271508+jvdhaterdwestland@users.noreply.github.com>
2023-08-21 13:07:14 +0000

    aca jobs

--- caf_solution/add-ons/github_runners_aca_jobs/container_app_jobs.tf
+++ caf_solution/add-ons/github_runners_aca_jobs/container_app_jobs.tf
@@ -5,7 +5,7 @@ module "container_app_jobs" {
 
   name                                = each.value.name
   client_config                       = module.caf.client_config
-  container_app_environment_id        = can(each.value.container_app_environment_id) ? each.value.container_app_environment_id : module.container_app_environments[try(each.value.container_app_environment.lz_key, var.landingzone.key)][try(each.value.container_app_environment.key, each.value.container_app_environment_key)].id
+  container_app_environment_id        = can(each.value.container_app_environment_id) ? each.value.container_app_environment_id : module.container_app_environments[try(each.value.container_app_environment.key, each.value.container_app_environment_key)].id
   location                            = can(local.global_settings.regions[each.value.region]) ? local.global_settings.regions[each.value.region] : local.combined.resource_groups[try(each.value.resource_group.lz_key, var.landingzone.key)][try(each.value.resource_group.key, each.value.resource_group_key)].location
   resource_group_id                   = can(each.value.resource_group.id) || can(each.value.resource_group_id) ? try(each.value.resource_group.id, each.value.resource_group_id) : local.combined.resource_groups[try(each.value.resource_group.lz_key, var.landingzone.key)][try(each.value.resource_group_key, each.value.resource_group.key)].id
   global_settings                     = local.global_settings
