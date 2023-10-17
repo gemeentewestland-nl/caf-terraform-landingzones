67057f0157ab5e86f9383b4574821e028917a1af
marcomansiwestland <134270083+marcomansiwestland@users.noreply.github.com>
2023-10-17 11:32:29 +0200

    Switching back to the official CAF, version 5.7.6

--- caf_solution/landingzone.tf
+++ caf_solution/landingzone.tf
@@ -1,7 +1,7 @@
 module "solution" {
-  #source  = "aztfmod/caf/azurerm"
-  #version = "5.7.5"
-  source = "git::https://github.com/gemeentewestland-nl/terraform-azurerm-caf.git?ref=westland"
+  source  = "aztfmod/caf/azurerm"
+  version = "5.7.6"
+  #source = "git::https://github.com/gemeentewestland-nl/terraform-azurerm-caf.git?ref=westland"
 
   providers = {
     azurerm.vhub = azurerm.vhub
