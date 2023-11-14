ac80b33268e39ad0942f3bdf65b5f2ec9f908336
marcomansiwestland <134270083+marcomansiwestland@users.noreply.github.com>
2023-11-14 17:00:02 +0000

    using westland repo and branch

--- caf_solution/landingzone.tf
+++ caf_solution/landingzone.tf
@@ -1,7 +1,7 @@
 module "solution" {
-  source  = "aztfmod/caf/azurerm"
-  version = "5.7.6"
-  #source = "git::https://github.com/gemeentewestland-nl/terraform-azurerm-caf.git?ref=westland"
+  #source  = "aztfmod/caf/azurerm"
+  #version = "5.7.6"
+  source = "git::https://github.com/gemeentewestland-nl/terraform-azurerm-caf.git?ref=feature/recovery-vault-extra-options"
 
   providers = {
     azurerm.vhub = azurerm.vhub
