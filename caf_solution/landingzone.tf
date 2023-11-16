4ee6ffccac547177ce58b26ac50c350b3dd485a5
marcomansiwestland <134270083+marcomansiwestland@users.noreply.github.com>
2023-11-16 15:19:59 +0000

    using another branch

--- caf_solution/landingzone.tf
+++ caf_solution/landingzone.tf
@@ -1,7 +1,7 @@
 module "solution" {
   #source  = "aztfmod/caf/azurerm"
   #version = "5.7.6"
-  source = "git::https://github.com/gemeentewestland-nl/terraform-azurerm-caf.git?ref=feature/recovery-vault-extra-options"
+  source = "git::https://github.com/gemeentewestland-nl/terraform-azurerm-caf.git?ref=feature/fix-lzkey-recoveryvault"
 
   providers = {
     azurerm.vhub = azurerm.vhub
